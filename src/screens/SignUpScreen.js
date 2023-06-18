938d2ca275f3a0f08052fc1deca85dcc64248860
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-06-19 01:42:21 +0530

    added trailers2

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -6,8 +6,8 @@ const SignUpScreen = ({ Email }) => {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const [enteredEmail, setEnteredEmail] = useState(Email);
- 
-  
+  const [isNewUser, setIsNewUser] = useState(false);
+
   const register = (e) => {
     e.preventDefault();
     auth
@@ -19,10 +19,12 @@ const SignUpScreen = ({ Email }) => {
         console.log(authUser);
       })
       .catch((error) => {
-        if(error.message === "Firebase: The email address is badly formatted. (auth/invalid-email).")
+        if (
+          error.message ===
+          "Firebase: The email address is badly formatted. (auth/invalid-email)."
+        )
           alert("Please enter correct Email and Password!");
-        else  
-          alert(error.message);
+        else alert(error.message);
       });
   };
   const signIn = (e) => {
@@ -36,19 +38,21 @@ const SignUpScreen = ({ Email }) => {
         // alert("Signed In");
       })
       .catch((error) => {
-        if(error.message === "Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found).")
+        if (
+          error.message ===
+          "Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found)."
+        )
           alert("Looks like you are new to Netflix, click on 'Sign-Up Now'");
-        else
-          alert(error.message);
+        else alert(error.message);
         // "'"
-        
       });
   };
 
   return (
     <div className="signupScreen">
       <form>
-        <h1>Sign In</h1>
+        {isNewUser ? <h1>Sign Up</h1> : <h1>Sign In</h1>}
+
         <input
           ref={emailRef}
           value={enteredEmail}
@@ -57,15 +61,50 @@ const SignUpScreen = ({ Email }) => {
           placeholder="Email"
         />
         <input ref={passwordRef} type="password" placeholder="Password" />
-        <button type="submit" onClick={signIn}>
-          Sign In
-        </button>
-        <h4>
-          <span className="signupScreen__gray">New to Netflix? </span>
-          <span className="signupScreen__link" onClick={register}>
-            Sign up now.
-          </span>
-        </h4>
+        {isNewUser ? (
+          <button
+            type="submit"
+            onClick={(e) => {
+              register(e);
+            }}
+          >
+            Sign Up
+          </button>
+        ) : (
+          <button
+            type="submit"
+            onClick={(e) => {
+              signIn(e);
+            }}
+          >
+            Sign In
+          </button>
+        )}
+        {isNewUser ? (
+          <h4>
+            <span className="signupScreen__gray">Already a Member? </span>
+            <span
+              className="signupScreen__link"
+              onClick={() => {
+                setIsNewUser(false);
+              }}
+            >
+              Sign in now.
+            </span>
+          </h4>
+        ) : (
+          <h4>
+            <span className="signupScreen__gray">New to Netflix? </span>
+            <span
+              className="signupScreen__link"
+              onClick={() => {
+                setIsNewUser(true);
+              }}
+            >
+              Sign up now.
+            </span>
+          </h4>
+        )}
       </form>
     </div>
   );
